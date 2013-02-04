25c6645adf05535d7fc2a0fdfaa802dcf63e3a43
Todd Gerspacher <toddgerspacher27@gmail.com>
2013-02-04 12:51:52 -0500

    Started testing ElementIterator.

--- src/test/ElementIteratorTest.java
+++ src/test/ElementIteratorTest.java
@@ -1,40 +1,44 @@
 package test;
 
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
 import java.util.NoSuchElementException;
 
-import net.datastructures.Position;
+import net.datastructures.ElementIterator;
+import net.datastructures.NodePositionList;
 import net.datastructures.PositionList;
+
+import org.junit.Test;
 
 public class ElementIteratorTest {
 
-}
+  @Test
+  public void testDummy() {
+    NodePositionList<String> list = new NodePositionList<String>();
+    assertThat(list.isEmpty(), is(equalTo(true)));
 
-//protected PositionList<E> list; // the underlying list
-//protected Position<E> cursor;   // the next position
-//
-///** Creates an element iterator over the given list. */
-//public ElementIterator(PositionList<E> L) {
-//  list = L;
-//  cursor = (list.isEmpty())? null : list.first();
-//}
-//
-///** Returns whether the iterator has a next object. */
-//public boolean hasNext() {
-//  return (cursor != null);
-//}
-//
-///** Returns the next object in the iterator. */
-//public E next() throws NoSuchElementException {
-//  if (cursor == null)
-//    throw new NoSuchElementException("No next element");
-//  E toReturn = cursor.element();
-//  cursor = (cursor == list.last())? null : list.next(cursor);
-//  return toReturn;
-//}
-//
-///** Throws an {@link UnsupportedOperationException} in all cases,
-// * because removal is not a supported operation in this iterator.
-// */
-//public void remove() throws UnsupportedOperationException {
-//  throw new UnsupportedOperationException("remove");
-//}
+    ElementIterator<String> iterator = new ElementIterator<String>(list);
+    assertThat(iterator.hasNext(), is(equalTo(false)));
+
+    try {
+      iterator.remove();
+      fail("Expected UnsupportedOperationException");
+    }
+    catch(UnsupportedOperationException unsupportedException) {
+      assertThat(unsupportedException.getMessage(), is(equalTo("remove")));
+    }
+
+    try {
+      iterator.next();
+      fail("Expected NoSuchElementException");
+    }
+    catch(NoSuchElementException noSuchElement) {
+      assertThat(noSuchElement.getMessage(), is(equalTo("No next element")));
+    }
+  }
+
+  // TODO: test with a non empty NodePositionList list
+}
