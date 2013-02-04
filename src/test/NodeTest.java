9ee9fd4cf72fa25310af21583e55875da320c39e
Todd Gerspacher <toddgerspacher27@gmail.com>
2013-02-04 08:18:05 -0500

    Implemented a unit test for Singly Linked List class.

---
+++ src/test/NodeTest.java
@@ -0,0 +1,36 @@
+package test;
+
+import net.datastructures.Node;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+import org.junit.Test;
+
+/** @author Todd Gerspacher */
+public class NodeTest {
+
+  @Test
+  public void testEmptyConstructor() {
+    Node<String> node = new Node<String>();
+
+    String element = node.getElement();
+    assertThat(element, is(equalTo(null)));
+
+    Node<String> next = node.getNext();
+    assertThat(next, is(equalTo(null)));
+  }
+
+  @Test
+  public void testParameterizedConstructor() {
+    Node<String> node = new Node<String>("Hello World", new Node<String>());
+
+    String element = node.getElement();
+    assertThat(element, is(equalTo("Hello World")));
+
+    Node<String> next = node.getNext();
+    assertThat(next.getElement(), is(equalTo(null)));
+    assertThat(next.getNext(), is(equalTo(null)));
+  }
+}
