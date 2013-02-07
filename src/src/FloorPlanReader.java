32f84ad9e5c7b55f86e86a5c104e698bea630c4f
Todd Gerspacher <toddgerspacher27@gmail.com>
2013-02-07 11:19:35 -0500

    Created a utility class for building a Floor Plan's Binary Tree from a preorder traversal.

---
+++ src/src/FloorPlanReader.java
@@ -0,0 +1,43 @@
+package src;
+
+import java.util.Stack;
+
+import tree.binary.Tree;
+import tree.binary.Node;
+
+public class FloorPlanReader {
+  public static Tree<Character> buildTree(String str) {
+    char ch;
+    Node<Character> curr = null;
+    Node<Character> node = null;
+    Stack<Node<Character>> stack = new Stack<Node<Character>>();
+    for(int i = 0; i < str.length(); i++) {
+      ch = str.charAt(i);
+      node = new Node<Character>();
+      node.setValue(ch);
+      if(null == curr) {
+        curr = node;
+      }
+      else {
+        while((null != curr.getLeftChild()) && (null != curr.getRightChild())) {
+          curr = stack.pop();
+        }
+        if(null == curr.getLeftChild()) {
+          curr.setLeftChild(node);
+        }
+        else {
+          curr.setRightChild(node);
+        }
+        if(('|' == ch) || ('-' == ch)) {
+          stack.push(curr);
+          curr = node;
+        }
+      }
+    }
+    Node<Character> root = stack.pop();
+    Tree<Character> tree = new Tree<Character>();
+    tree.setRoot(root);
+
+    return tree;
+  }
+}
