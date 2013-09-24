666abd8ce8f8e85c42f12e381a0bd855110a85bb
Todd Gerspacher <toddgerspacher27@gmail.com>
2013-09-24 06:54:12 -0400

    checkstyle warning: args parameter should be final

--- src/gui/MainPanel.java
+++ src/gui/MainPanel.java
@@ -75,7 +75,7 @@ public class MainPanel extends JPanel {
     nodes.addActionListener(actionListener);
   }
 
-  public static void main(String[] args) {
+  public static void main(final String[] args) {
     JFrame frame = new JFrame("FileChooserPanel");
     frame.setContentPane(new MainPanel());
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
